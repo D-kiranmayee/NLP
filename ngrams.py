a9dfd25619b2c99930f09f2ce0f9e2dbe9633699
Kiranmayee <kiranmayee.dobbali@gmail.com>
2019-09-12 13:17:04 -0600

    second commit

--- ngrams.py
+++ ngrams.py
@@ -2,8 +2,8 @@
 import sys
 from collections import Counter
 
-def unigrams(text_file):
-    file1 = open(text_file,"r")
+def unigrams(train_file):
+    file1 = open(train_file,"r")
     str = file1.read()
     str = str.lower()
     print(str)
@@ -25,13 +25,34 @@ def unigrams(text_file):
 
 def bigrams(text_file):
     file1 = open(text_file,"r")
-    str = file1.read()
+    str1 = file1.readlines()
+    list_sentence = []
+    list_bigrams=[]
+     ####### sending each sen into list
+    for i in str1:
+        list_sentence.append(i)
+
+    ###### changing into lower
+    list_sentence=[x.lower().strip() for x in list_sentence]
+    ###### adding # to start of each sentence
+    list_sentence = ["# "+li for li in list_sentence]
 
 
+    for l in list_sentence:
+        l1=l.split(" ");
+        print(l1)
+        for i in range(0, len(l1)-1):
+            bigrams=(l1[i],l1[i+1])
+            list_bigrams.append(bigrams);
 
 
+    dict_bigrams=Counter(list_bigrams)
+    #print(list_bigrams)
+    #print(dict_bigrams)
 
 def main():
-    unigrams(sys.argv[1])
+#    unigrams(sys.argv[1])
+    #bigrams(sys.argv[1])
+    bigrams(sys.argv[1])
     print("cmd line arg:",sys.argv[1])
 main()
