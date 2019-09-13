a622c55f556a6b1d8abeaaed31255cb2fe9a9be6
Kiranmayee <kiranmayee.dobbali@gmail.com>
2019-09-13 14:27:11 -0600

    final

--- ngram.py
+++ ngram.py
@@ -3,7 +3,7 @@ import sys
 import math
 from collections import Counter
 # total uni =16641
-
+import random
 
 
 def unigrams(text_file):
@@ -73,7 +73,7 @@ def bigrams(text_file):
         list_sentence.append(i)
 
     ###### changing into lower
-    list_sentence=[x.lower().strip() for x in list_sentence]
+    list_sentence=[x.lower().strip().replace("\n","") for x in list_sentence]
     ###### adding # to start of each sentence
     list_sentence = ["# "+li for li in list_sentence]
 
@@ -103,13 +103,14 @@ def bigram_unsmooth(test_file):
     dict_unigram = unigrams(sys.argv[1])
     total_vocab=sum(dict_unigram.values())
 
-    file_data = open(sys.argv[1],"r").read().split("\n")
+    file_data = open(sys.argv[1],"r").read().strip().split("\n")
+    #print(file_data)
     hash_len = len(file_data)
-
+    #print(len(file_data))
 
 
     dict_unigram["#"]=hash_len
-
+    #print("hash",hash_len)
     dict_bigrams = bigrams(sys.argv[1])
     #print(dict_bigrams)
     list_sentence3=[x.lower().strip().replace("\n","") for x in list_sentence3]
@@ -133,7 +134,10 @@ def bigram_unsmooth(test_file):
                 bi_prob="undefined"
                 break
             else:
-                bi_prob = bi_prob+ math.log(numer/denom,2)
+                #print(w[i])
+                #print(numer,"---",denom)
+                bi_prob = bi_prob+ math.log2(numer/denom)
+                #print(bi_prob)
         probs.append(bi_prob)
         #print("bigram unsmooth:",bi_prob)
     return probs
@@ -150,7 +154,7 @@ def bigram_smooth(test_file):
     dict_unigram = unigrams(sys.argv[1])
     total_vocab=len(dict_unigram)
 
-    file_data = open(sys.argv[1],"r").read().split("\n")
+    file_data = open(sys.argv[1],"r").read().strip().split("\n")
     hash_len = len(file_data)
 
 
@@ -180,6 +184,79 @@ def bigram_smooth(test_file):
         #print("bigram smooth:",bi_prob)
     return probs
 
+def lang_generator(bseed,count):
+    #bseed='she'
+    dict_bigrams = bigrams(sys.argv[1])
+    list_words_1=[]
+    list_words_2=[]
+    prob_bi={}
+    counter=0
+    sum=0
+    w=[]
+    #list_keys = dict_bigrams.keys()
+    #print(list_keys[0])
+    for key in dict_bigrams:
+        tup_words=key
+        list_words_1.append(tup_words[0])
+        list_words_2.append(tup_words[1])
+        #if bseed in list_words_1:
+            #print("yes")
+    #print(list_words_1)
+    b_seed = bseed.lower()
+    #print("bseed",bseed)
+    if b_seed in list_words_1:
+        indices = [i for i, x in enumerate(list_words_1) if x == b_seed]
+
+    for ix in indices:
+        vals= dict_bigrams[(list_words_1[ix],list_words_2[ix])]
+        counter+=vals
+    for ix in indices:
+        vals= dict_bigrams[(list_words_1[ix],list_words_2[ix])]
+        #print(list_words_1[ix],"---",list_words_2[ix],"====",vals)
+        freq = vals/counter
+        prob_bi.update({(list_words_1[ix],list_words_2[ix]): freq})
+        #prob_bi.update({'(list_words_1[ix],list_words_2[ix])','vals'})
+#    print(prob_bi)
+
+    #print(counter)
+    #print(prob_bi)
+
+    #for x in range(0,10):
+    for i in range(0,1):
+        rand_num=random.uniform(0,1)
+        #print("rand-number",rand_num)
+        for key in prob_bi:
+            val= prob_bi[key]
+            #print(key[1])
+            sum+= val
+            #print("n----",val)
+            #print(w1)
+            if(rand_num<sum):
+                #w.append(key[0])
+                print(key[0],end=" ")
+                #print(key[0])
+                break
+        if(key[0]!='.' and key[0]!='?' and key[0]!='!' and count<10 and bseed!=""):
+            count+=1
+            #print(key[0],"--",key[1])
+            lang_generator(key[1],count)
+        else:
+            return
+    #print()
+
+
+def seed_test_data():
+    file_seed = open(sys.argv[5],"r").read()
+    sent = file_seed.split("\n")
+    #print(sent)
+    for w in sent:
+        print("Seed =",w)
+        print()
+        for i in range(0,10):
+            print("Sentence",i+1,":",end=" ")
+            lang_generator(w,0)
+            print()
+        print()
 
 def main():
     file_main = open(sys.argv[3],"r").read()
@@ -191,14 +268,14 @@ def main():
     for i in range(0,(sen_len)):
         print("S =", sent[i])
         print("\n")
-        print("Unsmoothed Unigrams, logprob(S){:+.4f}".format(round(uni_probs_un[i],4)))
+        print("Unsmoothed Unigrams, logprob(S)= {:.4f}".format(round(uni_probs_un[i],4)))
 
         if(bi_probs_un[i]=='undefined'):
-            print("Unsmoothed Bigrams, logprob(S)",bi_probs_un[i])
+            print("Unsmoothed Bigrams, logprob(S)= ",bi_probs_un[i])
         else:
-            print("Unsmoothed Bigrams, logprob(S){:+.4f}".format(bi_probs_un[i]))
+            print("Unsmoothed Bigrams, logprob(S)= {:.4f} ".format(bi_probs_un[i]))
 
-        print("Smoothed Bigrams, logprob(S){:+.4f}".format(bi_probs_sm[i]))
+        print("Smoothed Bigrams, logprob(S)= {:.4f}".format(bi_probs_sm[i]))
         print("\n")
-
+    seed_test_data()
 main()
